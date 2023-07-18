c04d5fed7c6fcb958a301f9572e66f04b84454c7
Prateek Tandon <erprateek.vit@gmail.com>
2023-07-17 19:48:50 -0700

    Adding all needed steps

--- execute_workflow.sh
+++ execute_workflow.sh
@@ -4,7 +4,9 @@ set -euxo pipefail
 
 TRIM_GALORE_OUTPUTS=~/trim_galore/
 FASTQC_OUTPUTS_DIR=~/fastqc_outputs_post_trimming/
-
+SOURMASH_OUTPUTS=~/sourmash_outputs/
+REFERENCES_DIR=~/references/
+PIPELINE_OUTPUTS=~/outputs
 print_usage() {
   echo "Usage: $0 -1 <read1.fq.gz> -2 <read2.fq.gz>"
 }
@@ -60,4 +62,33 @@ fi
 echo "Obtained read1 file: $read1"
 echo "Obtained read2 file: $read2"
 mkdir -p $FASTQC_OUTPUTS_DIR
-trim_galore --output $TRIM_GALORE_OUTPUTS --fastqc --fastqc_args "--outdir $FASTQC_OUTPUTS_DIR --extract" --paired $read1 $read2 
+mkdir -p $TRIM_GALORE_OUTPUTS
+mkdir -p $SOURMASH_OUTPUTS
+mkdir -p $PIPELINE_OUTPUTS
+
+trim_galore --output $TRIM_GALORE_OUTPUTS --fastqc --fastqc_args "--outdir $FASTQC_OUTPUTS_DIR --extract" --paired $read1 $read2
+R1_TRIMMED=$TRIM_GALORE_OUTPUTS/
+
+SOURMASH_SIGNATURE_FILE=sample.sig.gz
+SOURMASH_K=31
+SOURMASH_OUTPUT_CSV=sourmash_output.csv
+sourmash sketch dna -p k=$SOURMASH_K,abund $TRIM_GALORE_OUTPUTS/read*.fastq.gz -o $SOURMASH_OUTPUTS/$SOURMASH_SIGNATURE_FILE --name new_sample
+sourmash gather $SOURMASH_SIGNATURE_FILE gtdb-rs207.genomic-reps.dna.k$SOURMASH_K.zip --output $SOURMASH_OUTPUTS/$SOURMASH_OUTPUT_CSV
+
+
+## Download reference
+mkdir -p $REFERENCES_DIR
+NCBI_ID=$(cut -d',' -f 10 $SOURMASH_OUTPUT_CSV | grep -v name | head -1 | sed 's/"//g' | cut -d " " -f 1);
+curl -o $REFERENCES_DIR -OJX GET "https://api.ncbi.nlm.nih.gov/datasets/v2alpha/genome/accession/$NCBI_ID/download?include_annotation_type=GENOME_FASTA&filename=${NCBI_ID}.zip" -H "Accept: application/zip" 
+unzip $REFERENCES_DIR/$NCBI_ID.zip
+REFERENCE=$REFERENCES_DIR/ncbi_dataset/data/$NCBI_ID/$NCBI_ID_genomic.fna
+
+## BWA Index
+bwa index $REFERENCE
+
+## Run Alignment
+bwa mem $REFERENCE $TRIM_GALORE_OUTPUTS/*1.fq.gz $TRIM_GALORE_OUTPUTS/*2.fq.gz | samtools view -bS $PIPELINE_OUTPUTS/sample.bam
+
+## Create plot
+python generate_report.py -f $FASTQC_OUTPUTS_DIR -t $TRIM_GALORE_OUTPUTS -s $SOURMASH_OUTPUTS -b $PIPELINE_OUTPUTS/sample.bam -o $PIPELINE_OUTPUTS/report.csv
+#python create_plot.py -b $PIPELINE_OUTPUTS/sample.bam -1 $read1 -2 $read2 -o $PIPELINE_OUTPUTS/plot.png
--- generate_report.py
+++ generate_report.py
@@ -1,15 +1,80 @@
+#!/bin/python
+
 import os
 import sys
+import glob
 import argparse
 import pandas as pd
 
+def get_organism_from_sourmash(sourmash_csv):
+    df = pd.read_csv(sourmash_csv)
+    name = df['name'].iloc[0]
+    name_split = df['name'].split("=")[0]
+    ncbi_id = name_split.split(" ")[0]
+    organism_name = name_split.replace(ncbi_id, "").replace("strain","").lstrip().rstrip()
+    return (ncbi_id, organism_name)
+
+def get_insert_size_mean_and_std():
+    isize_mean = 0
+    isize_std = 0
+    return (isize_mean, isize_std)
+
+def parse_trimgalore_outputs(trimgalore_output_dir):
+    reports = sorted(glob.glob(os.path.join(trimgalore_output_dir, "report.txt")))
+    content = []
+    with open(reports[0]) as tfile:
+        content = tfile.readlines()
+    adapter_sequence = ""
+    library_type = "Unknown"
+    for line in content:
+        if line.startswith("Adapter sequence"):
+            adapter_sequence = line.split(" ")[2].replace("'","")
+            if "nextera" in line.lower():
+                library_type = "Nextera"
+            elif "truseq" in line.lower():
+                library_type = "Truseq"
+    return (adapter_sequence, library_type)
+
+def get_sequencing_configuration():
+    r1_len = 0
+    r2_len = 0
+    i1_len = 0
+    i2_len = 0
+    return (r1_len, r2_len, i1_len, i2_len)
+
+def get_num_molecules():
+    num_molecules = 0
+    return num_molecules    
+    
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("-f",'--fastqc_outputs_dir')
     parser.add_argument("-t"."--trimgalore_output_dir")
+    parser.add_argument('-s','--sourmash_csv')
+    parser.add_argument("-b",'--sample_bam')
     parser.add_argument("-o","--output_file")
     
+    args = parser.parse_args()
     
+    isize_mean, isize_std = get_insert_size_mean_and_std()
+    ncbi_id, name = get_organism_from_sourmash(args.sourmash_csv)
+    r1_len, r2_len, i1_len, i2_len = get_sequencing_configuration()
+    adapter, library = parse_trimgalore_outputs(args.trimgalore_output_dir)
+    nmolecules = get_num_molecules()
+    row = [nmolecules, r1_len, r2_len, i1_len, i2_len, library, isize_mean, isize_std, ncbi_id]
+    cols = [
+        'Num_molecules',
+        'R1_length',
+        'R2_length',
+        'Index1_length',
+        'Index2_length',
+        'Library',
+        'Insert_size_mean',
+        'Insert_size_standard_deviation',
+        'NCBI_ID'
+    ]
+    df = pd.DataFrame(row, columns=cols)
+    df.to_csv(args.output_file, index=False)
 
 if __name__ == '__main__':
     main()
