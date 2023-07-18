f44a589c8f5dc51d7bdd7c7f31f132b57348a834
Prateek Tandon <erprateek.vit@gmail.com>
2023-07-17 18:09:21 -0700

    Adding return to source dir cd command

--- prepare_sourma.sh
+++ prepare_sourma.sh
@@ -8,3 +8,4 @@ sourmash sig summarize gtdb-rs207.genomic-reps.dna.k31.zip;
 gunzip gtdb-rs207.taxonomy.csv.gz;
 sourmash tax prepare -t gtdb-rs207.taxonomy.csv -o gtdb-rs207.taxonomy.sqldb -F sql
 export SOURMASH_SQLDB=$(pwd)/gtdb-rs207.taxonomy.sqldb;
+cd ~;
